3f22df599ead4056ede57fde89d478a6cd71285b
KimMancs <kckm317@naver.com>
2021-11-09 23:24:06 +0900

    first commit

---
+++ src/main/java/iducs/jsp/demo1/HelloApplication.java
@@ -0,0 +1,9 @@
+package iducs.jsp.demo1;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
